f83eb20b0ad8892cb9d421cd47219a5c1fe86713
phh235 <hoangaatrox23504@gmail.com>
2023-12-02 22:22:29 +0700

    Hoàng push

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author HOANG
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
